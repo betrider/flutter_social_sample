de84d3a356c5eb52f9199608e83d0dc75a535453
betrider <tlsfpem@gmail.com>
2023-02-01 11:48:13 +0900

    구글로그인 구현

---
+++ lib/social/sign_in_with_github.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:github_sign_in/github_sign_in.dart';
+
+Future<UserCredential> signInWithGitHub(BuildContext context) async {
+  // Create a GitHubSignIn instance
+  final GitHubSignIn gitHubSignIn = GitHubSignIn(
+    clientId: 'clientId',
+    clientSecret: 'clientSecret',
+    redirectUrl: 'https://my-project.firebaseapp.com/__/auth/handler',
+  );
+
+  // Trigger the sign-in flow
+  final result = await gitHubSignIn.signIn(context);
+
+  // Create a credential from the access token
+  final githubAuthCredential = GithubAuthProvider.credential(result.token!);
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(githubAuthCredential);
+}
