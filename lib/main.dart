216245297d83cc2aafa53e980c5afd36c3f9ddce
betrider <tlsfpem@gmail.com>
2023-02-01 16:52:22 +0900

    google 로그인 ios 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_social_sample/firebase_options.dart';
 import 'package:flutter_social_sample/social/sign_in_with_apple.dart';
 import 'package:flutter_social_sample/social/sign_in_with_facebook.dart';
 import 'package:flutter_social_sample/social/sign_in_with_github.dart';
@@ -10,7 +11,7 @@ import 'package:flutter_social_sample/social/sign_in_with_naver.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(name: 'test', options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
